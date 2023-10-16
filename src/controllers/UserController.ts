39fee7ea958b218fb8c473600f0573c7bf404028
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-10-16 19:00:13 -0300

    fix  error: when no image is uploaded the router trigger an error 504

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -60,8 +60,12 @@ class UserController {
   async updateProfile(req: Request, res: Response, next: NextFunction) {
     const { username } = req.user
     const { nickname, bio } = req.body
-    const { icon, banner } = req.body.uploadedUrls
-
+    const { icon, banner } = req.body.uploadedUrls || {}
+    console.log(username)
+    console.log(nickname)
+    console.log(bio)
+    console.log(icon)
+    console.log(banner)
     try {
       await this.userUseCase.updateProfile(username, nickname, bio, icon, banner)
       return res.status(200).json({ message: 'Profile updated' })
